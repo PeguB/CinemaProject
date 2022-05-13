7ce06a48ab3f2cc9e9020db66bd32b43bd62f948
mpanduru <mariuspanduru588@gmail.com>
2022-05-13 17:10:59 +0300

    CRA-6, created fxml and controller for a single movie entity

---
+++ src/main/java/org/fis/cinemaregistrationapplication/controllers/MovieInformationController.java
@@ -0,0 +1,20 @@
+package org.fis.cinemaregistrationapplication.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class MovieInformationController {
+
+    @FXML
+    private Text movieName;
+
+    @FXML
+    private Text movieDuration;
+
+    @FXML
+    private Text movieHour;
+
+    @FXML
+    private Text movieSeats;
+
+}
